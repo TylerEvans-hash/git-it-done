4517e13cbfbbd748053b8f4d1c314cd76f95dd2d
Red3ffect <red3ffect117@gmail.com>
2021-05-06 15:25:15 -0700

    Added error handling functionality and issues that ay occur

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,28 +1,47 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
 
-var getRepoIssues = function(repo) {
+var getRepoIssues = function (repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
     fetch(apiUrl).then(function (response) {
         // request was successful
         if (response.ok) {
-            response.json().then(function(data) {
+            response.json().then(function (data) {
                 displayIssues(data);
+
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         } else {
-            alert("There was a problem with your request!");
+            document.location.replace("./index.html");
         }
     });
 
     console.log(repo);
 };
 
-var displayIssues = function(issues) {
+var getRepoName = function () {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if (repoName) {
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    } else {
+        document.location.replace("./index.html")
+    }
+};
+
+var displayIssues = function (issues) {
     if (issues.length === 0) {
         issueContainerEl.textContent = "This repo has no open issues!";
         return;
     }
 
-    for (var i=0; i<issues.length;i++) {
+    for (var i = 0; i < issues.length; i++) {
         // create a link element to take users to the issue on GitHub
         var issueEl = document.createElement("a");
         issueEl.classList = "list-item flex-row justify-space-between align-center";
@@ -53,5 +72,18 @@ var displayIssues = function(issues) {
     };
 };
 
-getRepoIssues("TylerEvans-hash/run-buddy");
-// getRepoIssues("facebook/react");
+var displayWarning = function (repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+  
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
+
+getRepoIssues();
+getRepoName();
